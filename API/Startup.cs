5c5ce860755d4b77affcea4048be86dc9f8784c7
Sam <samshum90@gmail.com>
2020-12-06 14:28:44 +0000

    added exception middleware to API

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -52,11 +53,14 @@ namespace API
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-// ordering very important here
+            //replaces app.UseDeveloperExceptionPage()
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            // ordering very important here
             app.UseHttpsRedirection();
 
             app.UseRouting();
